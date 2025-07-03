6103b9313f6ab9218859308d39c4254db9013d57
dhanrajkale410 <dhanrajkale999@gmail.com>
2025-07-03 11:44:15 +0530

    Assignments & MarkDowns Files Updated

---
+++ Assignments/JS/Day 22 - setInterval, setTimeout, callback/TIme Clock/script.js
@@ -0,0 +1,21 @@
+// Time Clock
+
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    let hours = document.getElementById("hours")
+    hours.textContent = h;
+    let minutes = document.getElementById("minutes")
+    minutes.textContent = m;
+    let seconds = document.getElementById("seconds")
+    seconds.textContent = s;
+    setTimeout(startTime, 1000);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
